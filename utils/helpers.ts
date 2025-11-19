8371b5de3640c0b07720ce3d90738325e8b0d453
matransformlab <matransformlab@gmail.com>
2025-11-19 18:52:17 +0300

    Create helpers.ts

---
+++ utils/helpers.ts
@@ -0,0 +1,18 @@
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(amount / 100) // Convert cents to dollars
+}
+
+export function formatDate(date: string): string {
+  return new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+}
+
+export function classNames(...classes: string[]): string {
+  return classes.filter(Boolean).join(' ')
+}
